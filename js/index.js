3bd6f7a6169ae2f6d649b467c5ba7ff8edc0200a
Wes-White <wesleyrwhite86@gmail.com>
2019-04-10 18:46:37 -0700

    finished site content

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ const siteContent = {
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
-  "main-content": {
+  "mainContent": {
     "features-h4":"Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "about-h4":"About",
@@ -37,15 +37,61 @@ const siteContent = {
   },
 };
 
+
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
+// Nav and Banner Content 
+
 let menu = document.querySelectorAll('a')
-menu[0].textContent = siteContent.nav["nav-item-1"];
-menu[1].textContent = siteContent.nav["nav-item-2"];
-menu[2].textContent = siteContent.nav["nav-item-3"];
-menu[3].textContent = siteContent.nav["nav-item-4"];
-menu[4].textContent = siteContent.nav["nav-item-5"];
-menu[5].textContent = siteContent.nav["nav-item-6"];
+menu[0].textContent = siteContent.nav["nav-item-1"]
+menu[1].textContent = siteContent.nav["nav-item-2"]
+menu[2].textContent = siteContent.nav["nav-item-3"]
+menu[3].textContent = siteContent.nav["nav-item-4"]
+menu[4].textContent = siteContent.nav["nav-item-5"]
+menu[5].textContent = siteContent.nav["nav-item-6"]
+
+let head = document.querySelector('h1')
+head.textContent = siteContent.cta['h1']
+
+let bannerLogo = document.querySelector('#cta-img');
+bannerLogo.setAttribute('src', siteContent['cta']['img-src'])
+
+let button = document.querySelector('button')
+button.textContent = siteContent.cta['button']
+
+//Middle Content 
+
+let midContentHeaders = document.querySelectorAll('h4')
+midContentHeaders[0].textContent = siteContent.mainContent['features-h4']
+midContentHeaders[1].textContent = siteContent.mainContent['about-h4']
+midContentHeaders[2].textContent = siteContent.mainContent['services-h4']
+midContentHeaders[3].textContent = siteContent.mainContent['product-h4']
+midContentHeaders[4].textContent = siteContent.mainContent['vision-h4']
+
+let midContentText = document.querySelectorAll('.text-content p')
+midContentText[0].textContent = siteContent.mainContent['features-content']
+midContentText[1].textContent = siteContent.mainContent['about-content']
+midContentText[2].textContent = siteContent.mainContent['services-content']
+midContentText[3].textContent = siteContent.mainContent['product-content']
+midContentText[4].textContent = siteContent.mainContent['vision-content']
+
+let midImg = document.querySelector('#middle-img')
+midImg.setAttribute('src', siteContent ['mainContent']['middle-img-src'])
+
+
+//Bottom Section (Contact)
+
+let contactHeader = document.querySelector('.contact h4')
+contactHeader.textContent = siteContent.contact['contact-h4']
+
+let contactContent = document.querySelectorAll('.contact p')
+contactContent[0].textContent = siteContent.contact['address']
+contactContent[1].textContent = siteContent.contact['phone']
+contactContent[2].textContent = siteContent.contact['email']
+
+//Footer 
+let foot = document.querySelector('footer')
+foot.textContent = siteContent.footer.copyright
 
